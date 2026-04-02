b20d72b7b89e2cbe672b3891ff239a15957e2abb
RonnyWWW <ronsdhawan@gmail.com>
2026-04-02 15:19:07 -0500

    Update strip_node_patched.py

--- strip_map/strip_node_patched.py
+++ strip_map/strip_node_patched.py
@@ -134,8 +134,8 @@ class StripMapper(Node):
         self.get_logger().info(f"Received scan with {len(msg.ranges)} points")
 
         # --- Restrict Field of View (optional) ---
-        ANGLE_START_DEG = 50.0   # start of right side window
-        ANGLE_END_DEG   = 85.0     # up to center
+        ANGLE_START_DEG = 45.0   # middle (lower-> closer to middle)
+        ANGLE_END_DEG   = 85.0     #edge (higher -> closer to edge)
 
         angles_full = np.linspace(msg.angle_min, msg.angle_max, len(msg.ranges), dtype=np.float32)
         
