c674df1002de0eeaf6579c1a48ed2105ff9c2e40
RonnyWWW <ronsdhawan@gmail.com>
2026-04-06 16:09:20 -0500

    Update shelf_scanner_single.launch.py

--- launch/shelf_scanner_single.launch.py
+++ launch/shelf_scanner_single.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'scan_topic': '/sick_tim_5xx/scan',
-                'enable_velocity_sync': True,
+                'enable_velocity_sync': False,
                 'velocity_topic': '/odom',
                 'pixels_per_meter': 200.0,
                 'min_velocity_threshold': 0.01
